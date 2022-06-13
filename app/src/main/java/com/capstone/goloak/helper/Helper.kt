9b5d0c9526cfc619bf6410ab38b214aa0de1f22c
ihsan <ihsanmukhlis41@gmail.com>
2022-06-13 10:37:27 +0700

    Update date format

--- app/src/main/java/com/capstone/goloak/helper/Helper.kt
+++ app/src/main/java/com/capstone/goloak/helper/Helper.kt
@@ -12,6 +12,7 @@ fun String.withDateFormat(): String {
     val getFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale("in", "ID"))
     getFormat.timeZone = TimeZone.getTimeZone("GMT")
     val outputFormat = SimpleDateFormat("HH:mm:ss, dd MMMM yyyy", Locale("in", "ID"))
+
     val date = getFormat.parse(this) as Date
     outputFormat.timeZone = TimeZone.getDefault()
     return outputFormat.format(date)
